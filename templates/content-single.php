b4773c8130c8cf8efcc95c67722ce2f468f84da0
Enes Huseinčehajić <bezbeli@gmail.com>
2018-06-06 15:43:16 +0200

    refactor

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,27 +1,37 @@
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <?php if (has_post_thumbnail()) { ?>
-      <div class="featured-image">
-      <?php get_template_part('templates/part-ocjena-single');
-      the_post_thumbnail('featured-image', array( 'class' => 'img-responsive' ));
-      $thumbnail_id    = get_post_thumbnail_id($post->ID);
-      $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
-      if ($thumbnail_image && isset($thumbnail_image[0])) {
-        echo '<span class="absolute-bottom-right caption">' . $thumbnail_image[0]->post_content.'</span>';
-      }?>
-      </div>
-    <?php } ?>
-      <div class="article-content">
+<?php while (have_posts()) :
+    the_post(); ?>
+<article <?php post_class(); ?>>
+    <?php if (has_post_thumbnail()) :
+    ?>
+    <div class="featured-image">
+        <?php get_template_part('templates/part-ocjena-single');
+        the_post_thumbnail('featured-image', ['class' => 'img-responsive']);
+        $thumbnail_id = get_post_thumbnail_id($post->ID);
+        $thumbnail_image = get_posts(['p' => $thumbnail_id, 'post_type' => 'attachment']);
+        if ($thumbnail_image && isset($thumbnail_image[0])) :
+            echo '<span class="absolute-bottom-right caption">'.$thumbnail_image[0]->post_content.'</span>';
+        endif; ?>
+    </div>
+    <?php
+    endif; ?>
+    <div class="article-content">
         <header>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
-          <?php get_template_part('templates/entry-meta'); ?>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php get_template_part('templates/entry-meta'); ?>
         </header>
         <div class="entry-content">
-          <?php the_content(); ?>
+            <?php the_content(); ?>
         </div>
         <hr>
-        <?php get_template_part('templates/part-related-articles' );?>
-      </div>
-        <?php get_template_part('templates/part-featured-articles' );?>
-  </article>
+        <?php get_template_part('templates/part-related-articles'); ?>
+    </div>
+    <?php get_template_part('templates/part-featured-articles'); ?>
+</article>
 <?php endwhile; ?>
+<p>
+<time class="updated" datetime="<?= get_post_time('c', true); ?>"><span class="glyphicon glyphicon-calendar"></span>  <?= get_the_date(); ?></time>
+    <span class="glyphicon glyphicon-user"></span>
+    <?php __('Autor/ica:', 'sage');
+    echo get_field('autor') ? get_field('autor') : get_the_author();
+    ?>
+</p>
